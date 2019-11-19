3cab25b5eeef78370e0d4e8b669930dbdc94fc05
Gonzalo Moreno <goncab380@hotmail.com>
2019-11-19 20:22:43 -0300

    added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+job = JSON.parse(File.read("public/jobs.json"))["jobs"][0] 
+
+FavoriteJob.create slug: job["id"], url: "https://www.getonbrd.com#{job["url"]}", metadata: job.to_json
